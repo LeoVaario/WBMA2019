2d87ee10a71f18aa16f07dc4cad4b0ba4181a020
Leo Vaario <leovaa@metropolia.fi>
2019-01-17 14:18:50 +0200

    Http-a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
@@ -24,7 +26,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Pic } from '../../interfaces/pic';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  picArray: Pic[] = [];
+  mediaPath = 'assets/test.json';
+  constructor(public navCtrl: NavController, private http: HttpClient) {
   }
-
+  ngOnInit() {
+    this.getImages();
+  }
+  getImages() {
+    this.http.get<Pic[]>(this.mediaPath).subscribe((response: Pic[]) => this.picArray = response);
+    console.log(this.picArray);
+  }
 }
+
