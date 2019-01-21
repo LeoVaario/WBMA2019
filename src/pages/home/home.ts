6145447f575efeb8adae1edccc8ff646d256fdd1
Leo Vaario <leovaa@metropolia.fi>
2019-01-21 10:05:29 +0200

    first try commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
 import { Pic } from '../../interfaces/pic';
+import { MediaProvider } from '../../providers/media/media';
 
 @Component({
   selector: 'page-home',
@@ -10,22 +11,13 @@ import { Pic } from '../../interfaces/pic';
 
 export class HomePage {
   picArray: Pic[] = [];
-  mediaPathId = 'http://media.mw.metropolia.fi/wbma/media';
-  constructor(public navCtrl: NavController, private http: HttpClient) {
+
+  constructor(public navCtrl: NavController, private mediaProvider: MediaProvider) {
   }
   ngOnInit() {
-    this.getImages();
+    this.getAllFiles();
   }
-  getImages() {
-    this.http.get<Pic[]>(this.mediaPathId).
-      toPromise().
-      then((response: Pic[]) => {
-        Promise.all(response.map(
-          file => this.http.get<Pic>(this.mediaPathId + '/' + file.file_id).
-            toPromise())).then(result => {
-              this.picArray = result;
-              console.log(this.picArray);
-            });
-      });
+  getAllFiles() {
+    this.mediaProvider.getImages()
   }
 }
---
+++ src/providers/media/media.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Pic } from '../../interfaces/pic';
+
+/*
+  Generated class for the MediaProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class MediaProvider {
+
+  picArray: Pic[] = [];
+  mediaPathId = 'http://media.mw.metropolia.fi/wbma/media';
+  constructor(public http: HttpClient) {
+    console.log('Hello MediaProvider Provider');
+  }
+
+  getImages() {
+    this.http.get<Pic[]>(this.mediaPathId).
+      toPromise().
+      then((response: Pic[]) => {
+        Promise.all(response.map(
+          file => this.http.get<Pic>(this.mediaPathId + '/' + file.file_id).
+            toPromise())).then(result => {
+          this.picArray = result;
+          console.log(this.picArray);
+        });
+      });
+  }
+}
